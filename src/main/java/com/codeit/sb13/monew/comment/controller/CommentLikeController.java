2d30d8032cf99e0938745bb1844ec171bc25a221
hisjeans <his7jeans@gmail.com>
2026-08-21 17:33:57 +0900

    refactor: MID4-88 댓글 좋아요 등록 컨트롤러의 Swagger annotation을 별도의 인터페이스를 구성하여 분리

--- src/main/java/com/codeit/sb13/monew/comment/controller/CommentLikeController.java
+++ src/main/java/com/codeit/sb13/monew/comment/controller/CommentLikeController.java
@@ -3,11 +3,6 @@ package com.codeit.sb13.monew.comment.controller;
 import com.codeit.sb13.monew.comment.service.CommentLikeService;
 import com.codeit.sb13.monew.comment.service.dto.CommentLikeDto;
 import com.codeit.sb13.monew.comment.service.dto.CommentLikeRegisterCommand;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -21,29 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/comments")
-@Tag(name = "댓글 관리", description = "댓글 관련 API")
-public class CommentLikeController {
+public class CommentLikeController implements CommentLikeApi {
 
   private final CommentLikeService commentLikeService;
 
-  @Operation(
-      summary = "댓글 좋아요 등록",
-      description = "사용자가 특정 댓글에 좋아요를 누르는 기능입니다. 같은 사용자는 같은 댓글에 중복 좋아요를 누를 수 없습니다."
-  )
-  @Parameters({
-      @Parameter(
-          name = "commentId",
-          description = "좋아요를 누를 댓글의 ID",
-          required = true,
-          in = ParameterIn.PATH
-      ),
-      @Parameter(
-          name = "Monew-Request-User-ID",
-          description = "좋아요를 누른 요청자 ID",
-          required = true,
-          in = ParameterIn.HEADER
-      )
-  })
+  @Override
   @PostMapping("/{commentId}/comment-likes")
   public ResponseEntity<CommentLikeDto> likeComment(@PathVariable UUID commentId, @RequestHeader("Monew-Request-User-ID") UUID requestUserId) {
     CommentLikeRegisterCommand command = new CommentLikeRegisterCommand(commentId, requestUserId);
